760acac6a84910c8c97d4562010e73427b86ec54
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2021-10-06 18:46:29 +0200

    Equatable conformance added

--- Sources/StatefulUI/ViewState.swift
+++ Sources/StatefulUI/ViewState.swift
@@ -18,3 +18,37 @@ public enum ViewState<Value> {
         case cross(Value)
     }
 }
+
+extension ViewState.LoadingMode: Equatable where Value: Equatable {
+    
+    public static func == (lhs: ViewState.LoadingMode<Value>, rhs: ViewState.LoadingMode<Value>) -> Bool {
+        switch (lhs, rhs) {
+        case (.cover, .cover):
+            return true
+        case (.cross(let lhsValue), .cross(let rhsValue)):
+            return lhsValue == rhsValue
+        default:
+            return false
+        }
+    }
+    
+}
+
+extension ViewState: Equatable where Value: Equatable {
+    
+    public static func == (lhs: ViewState<Value>, rhs: ViewState<Value>) -> Bool {
+        switch (lhs, rhs) {
+        case (.idle, .idle):
+            return true
+        case (.loading(let lhsValue), .loading(let rhsValue)):
+            return lhsValue == rhsValue
+        case (.noContent, .noContent):
+            return true
+        case (.loaded(let lhsMode), .loaded(let rhsMode)):
+            return lhsMode == rhsMode
+        default:
+            return false
+        }
+    }
+    
+}
